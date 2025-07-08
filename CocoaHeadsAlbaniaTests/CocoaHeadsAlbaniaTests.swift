5687552366e7c3d94a59889d894e036d1b6de284
Bruno Frani <brunifrani@gmail.com>
2025-07-09 00:11:08 +0200

    Initial Commit

---
+++ CocoaHeadsAlbaniaTests/CocoaHeadsAlbaniaTests.swift
@@ -0,0 +1,17 @@
+//
+//  CocoaHeadsAlbaniaTests.swift
+//  CocoaHeadsAlbaniaTests
+//
+//  Created by Bruno Frani on 9.7.25.
+//
+
+import Testing
+@testable import CocoaHeadsAlbania
+
+struct CocoaHeadsAlbaniaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
